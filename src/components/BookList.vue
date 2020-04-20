350924cebcba6de984f85d0cd53f2751ead0918d
Nuno Coimbra <ncoimbra@gmail.com>
2020-04-20 17:46:27 +0100

    completed module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,18 +1,42 @@
 <template>
     <div>
         <h1>{{title}}</h1>
+        <ul>
+          <!-- <li>LOTR - Fellowship Of The Ring</li>
+          <li>LOTR - The Two Towers</li>
+          <li>LOTR - The Return Of The King</li> -->
+          <li v-for="bookm in books">{{book.title}}: {{book.author}}</li>
+        </ul>
     </div>
 </template>
 <script>
     export default {
       name: "BookList",
       data() {
-        return {title:"All Books"}
-    }
+        return
+        {
+          title:"All Books",
+          books: [
+              {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+              {title: 'American Gods', author: 'Neil Gaiman'},
+              {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+          ]
+        }
+      }
     }
 </script>
 <style>
     h1, h2 {
         font-weight: normal;
+    }
+
+    ul {
+      list-style-type: none;
+      padding:0;
+    }
+
+    li {
+      display:block;
+      margin 0 10px;
     }
 </style>
